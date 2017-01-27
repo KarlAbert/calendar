e25b2ea2e898dfa2491e773d5d964be3a58beee6
arstulke <arne.stulken@btc-ag.com>
2017-01-27 08:15:05 +0100

    Connection
    Signed-off-by: arstulke <arne.stulken@btc-ag.com>

---
+++ calendar/src/test/java/de/calendar/Connection.java
@@ -0,0 +1,88 @@
+package de.calendar;
+
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created Connection in de.calendar
+ * by ARSTULKE on 26.01.2017.
+ */
+public class Connection implements AutoCloseable {
+    private static final String DOMAIN = "http://localhost:8080";
+
+    private boolean readable = false;
+    private HttpURLConnection connection;
+
+    private Connection(URL url, Method method) throws IOException {
+        this.connection = (HttpURLConnection) url.openConnection();
+        this.connection.setRequestMethod(method.name());
+        readable = true;
+    }
+
+    static Connection open(String url, Method method) throws IOException {
+        return new Connection(formatURL(url), method);
+    }
+
+
+    Connection setAuthorizationHeader(String token) {
+        if (token != null) {
+            connection.setRequestProperty("Authorization", token);
+        }
+        return this;
+    }
+
+    Connection writeRequestBody(JSONObject data) throws IOException {
+        if (data != null) {
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.write(data.toString().getBytes());
+            }
+        }
+        return this;
+    }
+
+    Response readResponse() throws IOException {
+        if (readable) {
+            BufferedReader br;
+            if (connection.getResponseCode() == 404) {
+                throw new FileNotFoundException(String.format("Die Datei %s konnte nicht gefunden werden.", connection.getURL().toString()));
+            } else if ((connection.getResponseCode() < 200 || connection.getResponseCode() >= 300)) {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            }
+            String responseBody = "", line;
+            while ((line = br.readLine()) != null) {
+                responseBody += line;
+                responseBody += '\r';
+            }
+            br.close();
+            return new Response(connection.getResponseCode(), responseBody);
+        } else {
+            throw new IllegalStateException("You have to open the connection before you read its content.");
+        }
+    }
+
+    private static URL formatURL(String stringURL) throws MalformedURLException {
+        if (!DOMAIN.endsWith("/") && !stringURL.startsWith("/")) {
+            stringURL = "/" + stringURL;
+        }
+        return new URL(DOMAIN + stringURL.replaceAll(" ", "+"));
+    }
+
+    @Override
+    public void close() {
+        if (connection != null) {
+            connection.disconnect();
+        }
+    }
+
+    public enum Method {
+        POST, GET, PUT, DELETE
+    }
+}
--- calendar/src/test/java/de/calendar/TestUtils.java
+++ calendar/src/test/java/de/calendar/TestUtils.java
@@ -2,10 +2,7 @@ package de.calendar;
 
 import org.json.JSONObject;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
 
 /**
  * calendar:
@@ -14,95 +11,45 @@ import java.net.URL;
  */
 class TestUtils {
 
-    private static final String DOMAIN = "http://localhost:8080";
-
     static Response post(String url, String token, JSONObject data) {
-        return readResponse(sendRequest(url, token, data, "POST"));
+        try (Connection connection = Connection.open(url, Connection.Method.POST)) {
+            return connection
+                    .setAuthorizationHeader(token)
+                    .writeRequestBody(data)
+                    .readResponse();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     static Response put(String url, String token, JSONObject data) {
-        return readResponse(sendRequest(url, token, data, "PUT"));
+        try (Connection connection = Connection.open(url, Connection.Method.PUT)) {
+            return connection
+                    .setAuthorizationHeader(token)
+                    .writeRequestBody(data)
+                    .readResponse();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     static Response delete(String url, String token) {
-        return readResponse(sendRequest(url, token, null, "DELETE"));
+        try (Connection connection = Connection.open(url, Connection.Method.DELETE)) {
+            return connection
+                    .setAuthorizationHeader(token)
+                    .readResponse();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     static Response get(String url, String token) {
-        return readResponse(sendRequest(url, token, null, "GET"));
-    }
-
-    private static HttpURLConnection sendRequest(String url, String token, JSONObject data, String requestType) {
-        HttpURLConnection connection = null;
-        try {
-            connection = openConnection(requestType, url);
-            setAuthorizationHeader(connection, token);
-            writeRequestBody(connection, data);
-
-            return connection;
+        try (Connection connection = Connection.open(url, Connection.Method.GET)) {
+            return connection
+                    .setAuthorizationHeader(token)
+                    .readResponse();
         } catch (IOException e) {
-            closeConnection(connection);
             throw new RuntimeException(e);
         }
-    }
-
-    private static Response readResponse(HttpURLConnection connection) {
-        try {
-            BufferedReader br;
-            if (connection.getResponseCode() == 404) {
-                throw new FileNotFoundException(String.format("Die Datei %s konnte nicht gefunden werden.", connection.getURL().toString()));
-            } else if ((connection.getResponseCode() < 200 || connection.getResponseCode() >= 300)) {
-                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-            } else {
-                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            }
-            String responseBody = "", line;
-            while ((line = br.readLine()) != null) {
-                responseBody += line;
-                responseBody += '\r';
-            }
-            br.close();
-            return new Response(connection.getResponseCode(), responseBody);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            closeConnection(connection);
-        }
-    }
-
-    private static void closeConnection(HttpURLConnection connection) {
-        if (connection != null) {
-            connection.disconnect();
-        }
-    }
-
-    private static void setAuthorizationHeader(HttpURLConnection connection, String token) {
-        if (token != null) {
-            connection.setRequestProperty("Authorization", token);
-        }
-    }
-
-    private static void writeRequestBody(HttpURLConnection connection, JSONObject data) throws IOException {
-
-        if (data != null) {
-            connection.setRequestProperty("Content-type", "application/json");
-            connection.setDoOutput(true);
-            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-                wr.write(data.toString().getBytes());
-            }
-        }
-    }
-
-    private static HttpURLConnection openConnection(String httpMethod, String url) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) formatURL(url).openConnection();
-        connection.setRequestMethod(httpMethod);
-        return connection;
-    }
-
-    private static URL formatURL(String stringURL) throws MalformedURLException {
-        if (!DOMAIN.endsWith("/") && !stringURL.startsWith("/")) {
-            stringURL = "/" + stringURL;
-        }
-        return new URL(DOMAIN + stringURL.replaceAll(" ", "+"));
     }
 }
